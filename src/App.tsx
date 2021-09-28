b0bd59b8b11884071ef8ccc1114882ee741aa46c
leejss <goldemshine+3@gmail.com>
2021-09-28 14:37:13 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SingleSelect from "./components/Test";
+
+const App = () => {
+  return (
+    <div>
+      <SingleSelect />
+    </div>
+  );
+};
+
+export default App;
